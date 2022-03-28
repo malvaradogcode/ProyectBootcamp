a1838ea7753176d7a935673cb9d8d42fa9e880c7
Marco Alvarado <malvaradog@uni.pe>
2022-03-28 12:59:29 -0500

    Se terminó en análisis de los ejes de Consumo y Crecimiento energético

--- src/report.py
+++ src/report.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Reporte de ejemplo')
+st.title('Reporte: Consumo Global de Energía y Generación Renovable')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
